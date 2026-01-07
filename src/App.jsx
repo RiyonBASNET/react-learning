bc11dddbf7c9931bd0464e512bd317950ad859d3
riyonBASNET <basnet.riyon@gmail.com>
2026-01-07 21:07:43 +0545

    Add basic routing with React Router for Day pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import { Routes, Route, Link } from "react-router-dom";
+
 import ClickExample from "./components/ClickExample";
-import NameList from "./components/NameList";
 import Day1 from "./pages/Day1";
 import Day2 from "./pages/Day2";
 
@@ -8,12 +9,18 @@ function App() {
     <div>
       <h1>React Learning</h1>
 
-      <Day1 />
+      <nav>
+        <Link to="/day1">Day 1</Link>
+        <br />
+        <Link to="/day2">Day 2</Link>
+      </nav>
+      <Routes>
+        <Route path="/day1" element={<Day1 />} />
+        <Route path="/day2" element={<Day2 />} />
+      </Routes>
 
       <h2>Click</h2>
       <ClickExample />
-
-      <Day2 />
     </div>
   );
 }
