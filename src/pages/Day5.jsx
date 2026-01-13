ff376d2a90cdb8e48f9995e80d4a0ac16739dd4f
riyonBASNET <basnet.riyon@gmail.com>
2026-01-13 14:49:20 +0545

    Day 8: fetch novies using sideeffect with loading state

--- src/pages/Day5.jsx
+++ src/pages/Day5.jsx
@@ -1,15 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MovieList from "../components/MovieList";
 import AddMovieForm from "../components/AddMovieForm";
 
-const initialMovies = [
-  { id: 1, title: "Inception", year: 2010 },
-  { id: 2, title: "Interstellar", year: 2014 },
-  { id: 3, title: "Dune", year: 2021 },
-  { id: 4, title: "Deadpool", year: 2016 },
-];
 function Day5() {
-  const [movies, setMovies] = useState(initialMovies);
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setMovies([
+        { id: 1, title: "Inception", year: 2010 },
+        { id: 2, title: "Interstellar", year: 2014 },
+        { id: 3, title: "Dune", year: 2021 },
+        { id: 4, title: "Deadpool", year: 2016 },
+      ]);
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  if (loading) {
+    return <p>Loading movies...</p>;
+  }
 
   function handleAddMovie(movie) {
     setMovies((prevMovies) => [...prevMovies, movie]);
@@ -22,7 +33,7 @@ function Day5() {
   return (
     <div>
       <h1>Movie Library</h1>
-      <h2>Day 5 - Day 7</h2>
+      <h2>Day 5 - Day 8</h2>
 
       <AddMovieForm onAdd={handleAddMovie} />
       <MovieList movies={movies} onRemove={handleRemoveMovie} />
